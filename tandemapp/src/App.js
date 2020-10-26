29ddc548e840fdcb5d920106b5e187a41f63f5cc
Amesh Alex Kuruvilla <finixbenny@Ameshs-MacBook-Air.local>
2020-10-26 19:24:36 -0400

    added random question generation functionality + multiple choice buttons to answer

--- tandemapp/src/App.js
+++ tandemapp/src/App.js
@@ -1,13 +1,41 @@
 import QuestionData from './Tandem.json';
 
 function App() {
+
+  // const qArray = QuestionData.splice(0,10);
+  // console.log(qArray)
+
+  // function getRandomIntInclusive(min, max) {
+  //   min = Math.ceil(min);
+  //   max = Math.floor(max);
+  //   return Math.floor(Math.random() * (max - min + 1) + min); //The maximum is inclusive and the minimum is inclusive 
+  // }
+
+  // const rand = getRandomIntInclusive(0,9)
+
+  const item = QuestionData[Math.floor(Math.random() * QuestionData.length)];
+
+  // console.log(QuestionData, item)
+  
   return (
     <div className="App">
       <h1>
-        {QuestionData.map((question, idx)=> {
-          return <div> {console.log(question)}</div>;
-        })}
+        {item.question}
       </h1>
+      <button>
+        {item.correct}
+      </button>
+      
+        {item.incorrect.map((el)=> {
+          return (
+            <button>
+              {el}
+            </button>
+          )
+        })}
+        
+      
+      
     </div>
   );
 }
